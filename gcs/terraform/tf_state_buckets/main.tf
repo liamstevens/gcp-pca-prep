3f41fe91219b90339a0ba3675284c1f9fe6f13c4
Liam Stevens <me@liamstevens.me>
2021-12-29 09:25:22 +1100

    add stub storage statement

--- gcs/terraform/tf_state_buckets/main.tf
+++ gcs/terraform/tf_state_buckets/main.tf
@@ -1,3 +1,4 @@
 resource "google_storage_bucket" "tf_state_buckets" {
+  for_each = var.tf_buckets
   
 }
---
+++ gcs/terraform/tf_state_buckets/variables.tf
@@ -0,0 +1,4 @@
+variable "tf_bucket" {
+    description = "Names of all TF state buckets to be created."
+    type = list(string)
+}
