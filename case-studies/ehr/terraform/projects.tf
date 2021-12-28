fa5fbc78a5dbdff78c50bfe5e9fd34e994235b1a
Liam Stevens <me@liamstevens.me>
2021-12-29 08:36:43 +1100

    add further projects

--- case-studies/ehr/terraform/projects.tf
+++ case-studies/ehr/terraform/projects.tf
@@ -1,8 +1,17 @@
+resource "google_project" "billing_account" {
+  
+}
+
+resource "google_folder" "department1" {
+  display_name = "Department 1"
+  parent       = "organizations/1234567"
+}
+
 resource "google_project" "compute_project" {
   name       = "Compute Project"
   project_id = "ehr-compute-project"
   org_id     = var.organisation_id
-  billing_id = var.billing_id
+  billing_id = google_project.billing_account.id
   auto_create_network = false
 }
 
@@ -10,6 +19,14 @@ resource "google_project" "network_project" {
   name       = "Network Project"
   project_id = "ehr-network-project"
   org_id     = var.organisation_id
-  billing_id = var.billing_id
+  billing_id = google_project.billing_account.id
   auto_create_network = false
+}
+
+resource "google_project" "analytics_project" {
+  
+}
+
+resource "google_project" "database_project" {
+  
 }
