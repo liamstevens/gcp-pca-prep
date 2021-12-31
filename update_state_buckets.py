ac13cbe41de7322182b89a4a473e3e3b2e8e7bc8
Liam Stevens <me@liamstevens.me>
2021-12-31 23:15:09 +1100

    add method to update paths in tfvars file

--- update_state_buckets.py
+++ update_state_buckets.py
@@ -10,6 +10,25 @@ def get_terraform_paths(parent):
             tf_workspaces.append(str(e).rpartition('/')[-1]) 
         print(path.name)
 
+def update_bucket_workspace(paths,path='gcs/terraform/tf_state_buckets/my.tfvars'):
+    f = open(path, 'r')
+    tfvars = f.readlines()
+    f.close()
+    plist = []
+    for var in tfvars:
+        if 'tf_buckets' in var:
+            plist = list(var.rpartition('=')[-1].strip())
+            for p in paths:
+                if p not in plist:
+                    plist.append(p)
+                    var = 'tf_buckets = '+str(plist)
+    #Now write back the contents of the file plus the changes. this is really lazy and should be reworked at some point but is also not likely to be super slow.
+    f = open(path, 'w')
+    for var in tfvars:
+        f.write(var)
+            
+
+
 #Allow passing of a directory to traverse as the only argument to the command line. Else use the current working directory.
 if __name__ == '__main__':
     if len(sys.argv > 1):
