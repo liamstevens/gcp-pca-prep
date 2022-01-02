9798cd63580a598a41b83177e936a3c32d6ca378
Liam Stevens <me@liamstevens.me>
2022-01-02 13:50:23 +1100

    fix backend configuration for state bucket workspace

--- gcs/terraform/tf_state_buckets/backend.tf
+++ gcs/terraform/tf_state_buckets/backend.tf
@@ -7,17 +7,5 @@ terraform {
 
 provider "google" {
     project = var.state_storage_project
-    #impersonate_service_account = var.sa_email
     credentials = file(var.sa_creds)
 }
-
-# resource "google_project_service" "iam_credentials" {
-#     project = var.state_storage_project
-#     service = "iamcredentials.googleapis.com"
-# }
-
-# resource "time_sleep" "wait_30_seconds" {
-#   depends_on = [google_project_service.iam_credentials]
-
-#   create_duration = "30s"
-# }
