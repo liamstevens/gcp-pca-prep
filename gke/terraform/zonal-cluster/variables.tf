6bfdea526dfdeef7d342a43e9632acf161d28c9a
Liam Stevens <me@liamstevens.me>
2022-01-11 22:08:14 +1100

    add master cidr block var - not used at current

--- gke/terraform/zonal-cluster/variables.tf
+++ gke/terraform/zonal-cluster/variables.tf
@@ -8,3 +8,8 @@ variable "shared_vpc_id" {
   type        = string
   description = "The ID of the Shared VPC to use in cluster creation."
 }
+
+variable "master_cidr_block" {
+    type = string
+    description = "A CIDR block to be used for the control plane (it must be a /28 subnet)"
+}
