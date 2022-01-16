f83d32e69d1f7901913ac1aa672b2409c20006e2
Liam Stevens <me@liamstevens.me>
2022-01-16 23:14:48 +1100

    add gcs api

--- case-studies/ehr/terraform/ehr-sample-solution/api.tf
+++ case-studies/ehr/terraform/ehr-sample-solution/api.tf
@@ -21,9 +21,32 @@ resource "google_project_service" "gke_identity" {
 }
 
 resource "google_project_service" "gke_monitoring" {
-  project = google_project.compute_project.gke_identity
+  project = google_project.compute_project.id
   service = "monitoring.googleapis.com"
-  imeouts {
+  timeouts {
+    create = "30m"
+    update = "40m"
+  }
+
+  disable_dependent_services = true
+}
+
+resource "google_project_service" "gke_gcs" {
+  project = google_project.compute_project.id
+  service = "storage-component.googleapis.com"
+  timeouts {
+    create = "30m"
+    update = "40m"
+  }
+
+  disable_dependent_services = true
+}
+
+#### Analytics Project ####
+resource "google_project_service" "gke_gcs" {
+  project = google_project.analytics_project.id
+  service = "storage-component.googleapis.com"
+  timeouts {
     create = "30m"
     update = "40m"
   }
