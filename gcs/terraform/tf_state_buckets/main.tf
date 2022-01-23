932d9d17bf14d29f28c5e94ab43a6cf4d6771af5
Liam Stevens <me@liamstevens.me>
2022-01-23 19:55:27 +1100

    remove force_destroy, add create-only block to lifecycle for state buckets

--- gcs/terraform/tf_state_buckets/main.tf
+++ gcs/terraform/tf_state_buckets/main.tf
@@ -3,10 +3,13 @@ resource "google_storage_bucket" "tf_state_buckets" {
   
   name = each.key
   location      = "AUSTRALIA-SOUTHEAST1"
-  force_destroy = true
+  
 
   uniform_bucket_level_access = true
   versioning {
     enabled = true
+  }
+  lifecycle_rule {
+    ignore_changes = all
   }
 }
