139bea9f6ea544e5cdbef52789bd8b04b7a733e7
Liam Stevens <me@liamstevens.me>
2022-02-01 22:21:17 +1100

    add nat and interconnect

--- case-studies/ehr/terraform/ehr-sample-solution/network.tf
+++ case-studies/ehr/terraform/ehr-sample-solution/network.tf
@@ -24,3 +24,32 @@ resource "google_compute_subnetwork" "shared_subnetwork" {
     ip_cidr_range = "192.168.8.0/21"
   }
 }
+
+resource "google_compute_interconnect_attachment" "on_prem" {
+  name                     = "on-prem-attachment"
+  edge_availability_domain = "AVAILABILITY_DOMAIN_1"
+  type                     = "PARTNER"
+  router                   = google_compute_router.central_router.id
+  mtu                      = 1500
+}
+
+resource "google_compute_router" "central_router" {
+  name    = "router"
+  network = google_compute_network.foobar.name
+  bgp {
+    asn = 16550
+  }
+}
+
+resource "google_compute_router_nat" "nat" {
+  name                               = "egress-nat"
+  router                             = google_compute_router.central_router
+  region                             = google_compute_router.central_router.region
+  nat_ip_allocate_option             = "AUTO_ONLY"
+  source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
+
+  log_config {
+    enable = true
+    filter = "ERRORS_ONLY"
+  }
+}
