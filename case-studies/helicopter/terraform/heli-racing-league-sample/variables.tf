7dcbc256e8745db763d1b60dd4c820dbc3e6d27a
Liam Stevens <me@liamstevens.me>
2022-03-15 23:58:16 +1100

    terraform fmt

--- case-studies/helicopter/terraform/heli-racing-league-sample/variables.tf
+++ case-studies/helicopter/terraform/heli-racing-league-sample/variables.tf
@@ -2,8 +2,8 @@ variable "organisation_id" {
   description = "The ID of the parent organisation for folders and projects."
   type        = string
 }
-
+ 
 variable "region" {
-  description = "Region to deploy regional resources into"
-  type        = string
+    description = "Region to deploy regional resources into"
+    type        = string
 }
