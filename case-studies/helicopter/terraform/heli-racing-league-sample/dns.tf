968ecc3817ec69526a72ba9025d560625d83aa76
Liam Stevens <me@liamstevens.me>
2022-03-17 00:21:40 +1100

    fix dns resource associations

--- case-studies/helicopter/terraform/heli-racing-league-sample/dns.tf
+++ case-studies/helicopter/terraform/heli-racing-league-sample/dns.tf
@@ -6,8 +6,8 @@ resource "google_dns_managed_zone" "heli_domain" {
 
 
 resource "google_dns_record_set" "a" {
-  name         = "backend.${google_dns_managed_zone.prod.dns_name}"
-  managed_zone = google_dns_managed_zone.prod.name
+  name         = "api.${google_dns_managed_zone.heli_domain_api.dns_name}"
+  managed_zone = google_dns_managed_zone.heli_domain_api.name
   type         = "A"
   ttl          = 300
 
@@ -16,6 +16,6 @@ resource "google_dns_record_set" "a" {
 
 resource "google_dns_managed_zone" "heli_domain_api" {
   name        = "helicopter-racing-api"
-  dns_name    = "hrl-api.gcp-pca-prep.info"
+  dns_name    = "hrl-access.gcp-pca-prep.info"
   description = "Main API domain zone"
 }
